1e1bdd4f6a1b2bce2b670a6ae9d3b2acd9bb6d91
David De Potter <hyp3rbaton@gmail.com>
2022-10-04 21:31:59 +0200

    :tada:

---
+++ Finals/2018resit/problem3/prob3-2.c
@@ -0,0 +1,77 @@
+/* file: prob3.c
+   author: David De Potter
+   description: IP Final 2018 Resit, problem 3, ordered pairs
+   version: 2.0, fast version
+   We want to find the number of inversions in the array. 
+   In this version we sort the array with mergesort, and count the
+   number of inversions while merging. This approach gives a solution 
+   in O(nlogn).  
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int *copySubArray(int left, int right, int arr[]) {
+  int i, *copy = malloc((right - left)*sizeof(int));
+  for (i=left; i < right; i++) {
+    copy[i - left] = arr[i];
+  }
+  return copy;
+}
+
+int mergeSort(int length, int arr[]) {
+  int l, r, mid, idx, *left, *right, count = 0;
+  if (length <= 1) {
+    return 0;
+  }
+  mid = length/2;
+  left = copySubArray(0, mid, arr);
+  right = copySubArray(mid, length, arr);
+  count += mergeSort(mid, left);
+  count += mergeSort(length - mid, right);
+  idx = 0;
+  l = 0;
+  r = 0;
+  while ((l < mid) && (r < length - mid)) {
+    if (left[l] <= right[r]) {
+      // no inversions in this case
+      arr[idx] = left[l];
+      l++;
+    } else {
+      // total number of inversions is the 
+      // number of elements left in the left array
+      arr[idx] = right[r];
+      r++; count += (mid - l) ;
+    }
+    idx++;
+  }
+  while (l < mid) {
+    arr[idx] = left[l];
+    idx++;
+    l++;
+  }
+  while (r < length - mid) {
+    arr[idx] = right[r];
+    idx++;
+    r++;
+  }
+  free(left);
+  free(right);
+  return count; 
+}
+
+int *readIntVector (int size) {
+  int *vect = malloc(size * sizeof(int));
+  for (int i = 0; i < size; i++)
+    scanf("%d", vect + i);
+  return vect;
+}
+
+int main(int argc, char **argv){
+  int size; 
+  scanf("%d\n", &size);
+  int *vect = readIntVector(size); 
+  printf("%d\n", mergeSort(size, vect));
+  free(vect); 
+  return 0; 
+}
