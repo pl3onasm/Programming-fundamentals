d9939ee086347fdf4f1ade35eef4a5e523a1495f
David De Potter <hyp3rbaton@gmail.com>
2022-10-05 20:48:47 +0200

    :tada:

--- Midterms/mid2019resit/problem1/prob1.c
+++ Midterms/mid2019resit/problem1/prob1.c
@@ -1,4 +1,4 @@
-/* file: prob1.c
+/* file: prob1-2.c
 * author: David De Potter
 * description: IP mid2019 resit, problem 1,
 * binary representation
@@ -10,25 +10,23 @@
 int getHighestExponent (int n) {
   int exp = 0;
   while (n > 0) {
-    n /= 2; exp++;
+    n >>= 1; exp++;
   }
   return exp-1;
 }
 
 int main(int argc, char *argv[]) {
-  int n, bit;
-
+  int n;
   scanf("%d", &n);
   printf("%d=", n);
-
   int len = getHighestExponent(n);
 
-  for (int i = len; i >= 0; --i) {
-    int diff = n - 1<<i;
-    bit = diff >= 0 ? 1 : 0;
-    n = bit ? diff : n;
-    if (i == len) printf("%d*2^%d", bit, i);
-    else printf(" + %d*2^%d", bit, i);
+  for (int i=len; i>=0; i--) {
+    if (i<len) printf(" + ");
+    if (n >= 1<<i) {
+      printf("1*2^%d", i);
+      n -= 1<<i;
+    } else printf("0*2^%d", i);
   }
 
   printf("\n");
