af0557ebfbc8dab8fd39ae1d2aa6eb04154ef3d4
David De Potter <hyp3rbaton@gmail.com>
2022-10-09 15:43:55 +0200

    :tada:

--- Finals/2016/problem4/prob4.c
+++ Finals/2016/problem4/prob4.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   int a, b, parent[20] = {0,1,1,0,3,1,3,1,4,5,8,5,8,5,11,11,13,13,16,16};
   scanf("%d %d", &a, &b); 
   while (a != b) {
-    if  (a  == parent[a] || b == parent[b]) {
+    if  (a  == parent[a] && b == parent[b]) {
       printf("NONE\n");
       return 0; 
     }
