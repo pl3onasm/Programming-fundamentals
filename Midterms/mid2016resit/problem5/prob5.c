c1acd73d58173da775b25459222e387d988e35ba
David De Potter <hyp3rbaton@gmail.com>
2022-10-11 20:32:09 +0200

    :tada:

--- Midterms/mid2016resit/problem5/prob5.c
+++ Midterms/mid2016resit/problem5/prob5.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
       break;
     }
   }
-  if (valid == 1) {
+  if (valid) {
     if (checkPuzzle(n, puzzle)) printf("SOLVED\n");
     else printf("UNSOLVED\n");
   }
