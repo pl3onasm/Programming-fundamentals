c90640c57b47087b37ac2b1c65aeaddf7e646587
David <pl3onasm@gmail.com>
2022-10-13 16:08:49 +0200

    :tada:

--- Midterms/mid2022/problem1/myprogram.c
+++ Midterms/mid2022/problem1/myprogram.c
@@ -8,5 +8,12 @@
 
 int main(int argc, char *argv[]) {
   
+  // your code here
+
+  // to test your code, run the test script in the terminal: 
+  // $ ../../../ctest.sh myprogram.c
+  // after you made it executable by running: 
+  // $ chmod +x ../../../ctest.sh (you only need to do this once)
+  
   return 0;
 }
