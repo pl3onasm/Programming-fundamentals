d143e7d1430dd16bbbfe75e6b0a34ed5a1fd7d09
David <pl3onasm@gmail.com>
2022-10-13 15:46:15 +0200

    :tada:

--- Midterms/mid2018resit/problem5/prob5.c
+++ Midterms/mid2018resit/problem5/prob5.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 int modExp (int a, int b, int n) {
-  // computes a^b mod n using modular exponentiation 
+  // computes a^b mod n using modular exponentiation
   int r = 1;
   while (b > 0) {
     if (b % 2) r = (r * a) % n;
@@ -21,10 +21,11 @@ int main(int argc, char *argv[]) {
   int n, count=0;
   scanf ("%d", &n);
   for (int i=1; i<=n-2;++i) 
-    for (int j=i+1; j<=n-1; ++j) 
-      for (int k=j+1; k<=n; ++k) 
-        if ((i+j+k == n) && ((modExp(i,n,n)+modExp(j,n,n))%n == modExp(k,n,n)%n)) 
-          count++;
+    for (int j=i+1; j<=n-1; ++j){ 
+      int k = n-i-j; 
+      if (k > j && ((modExp(i,n,n)+modExp(j,n,n))%n == modExp(k,n,n)%n)) 
+        count++;
+    }
   printf("%d\n", count);
   return 0;
 }
