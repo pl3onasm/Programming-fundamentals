de64de24f18e76682ee0cad25131f4bf33098d3e
David <pl3onasm@gmail.com>
2022-10-28 14:03:15 +0200

    :tada:

--- Midterms/mid2018/problem3/prob3.c
+++ Midterms/mid2018/problem3/prob3.c
@@ -7,51 +7,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int row, x, y;
+  int row, x, y, flag=0;
   char col;
-  //reads in starting position
-  scanf("%c %d", &col, &row);
-  //reads coordinates of first move
-  scanf("%d %d", &x, &y);
+  scanf("%c %d", &col, &row); // starting position
 
-  while (x != 0 || y != 0) {
-    if (x != 0 && y != 0) {
-      //invalid move for a rook
-      printf("INVALID\n");
-      break;
-    }
-    if (x > 0) {
-      if (col+x <= 'h') col += x;
-      else { //out of boundaries
-        printf("INVALID\n");
-        break;
-      }
-    } else {  //x is negative
-      if (col+x >= 'a') col += x;
-      else {
-        printf("INVALID\n");
-        break;
-      }
-    }
-    if (y > 0) {
-      if (row+y <= 8) row += y;
-      else {
-        printf("INVALID\n");
-        break;
-      }
-    } else { //y is negative
-      if (row+y >= 1) row += y;
-      else {
-        printf("INVALID\n");
-        break;
-      }
-    }
-    //reads coordinates of next move
+  do {
     scanf("%d %d", &x, &y);
-  }
-  if ((x == 0) & (y == 0))
-    /* if true, scanning reached the last line
-     * without encountering any invalid moves */
-    printf("%c%d\n", col, row);
+    if (x != 0 && y != 0) break;
+    if (col+x <= 'h' && col+x >= 'a') col += x;
+    else break;
+    if (row+y <= 8 && row+y >= 1) row += y;
+    else break;
+  } while (x != 0 || y != 0);
+
+  if (x==0 && y==0) printf("%c%d\n", col, row);
+  else printf("INVALID\n");
   return 0;
 }
