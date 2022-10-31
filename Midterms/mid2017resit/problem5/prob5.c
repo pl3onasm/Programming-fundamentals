a0b95248b4279dc01ffda5efa0ca12b6ad715618
David <pl3onasm@gmail.com>
2022-10-31 23:45:28 +0100

    :tada:

--- Midterms/mid2017resit/problem5/prob5.c
+++ Midterms/mid2017resit/problem5/prob5.c
@@ -6,37 +6,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *a, int *b) {
-  int h;
-  h = *a;
-  *a = *b;
-  *b = h;
+void swap (int a[], int b[]) {
+  for (int i = 0; i < 2; i++) {
+    int temp = a[i];
+    a[i] = b[i];
+    b[i] = temp;
+  }
 }
 
 int main(int argc, char *argv[]) {
-  int rem1, rem2, rem3, mod1, mod2, mod3, i, g=0;
-
-  scanf("%d %d", &rem1, &mod1);
-  scanf("%d %d", &rem2, &mod2);
-  scanf("%d %d", &rem3, &mod3);
-  /* we want to get the maximum modulus, and perform
-   * any swaps as needed */
-  if (mod2 > mod1) {
-    swap(&mod1, &mod2);
-    swap(&rem1, &rem2);
-  }
-  if (mod3 > mod1) {
-    swap(&mod1, &mod3);
-    swap(&rem1, &rem3);
-  }
-  /* gets the smallest number that satisfies the equation
-   * that contains the largest modulus (divisor) */
+  int eq1[2], eq2[2], eq3[2], i, g=0;
+  scanf("%d %d", &eq1[0], &eq1[1]);
+  scanf("%d %d", &eq2[0], &eq2[1]);
+  scanf("%d %d", &eq3[0], &eq3[1]);
+  /* we want to get the maximum modulus */
+  if (eq1[1] > eq2[1]) swap (eq1, eq2);
+  if (eq1[1] > eq3[1]) swap (eq1, eq3);
+  /* now we want to get the smallest number that satisfies 
+   * tthe equation containing the largest modulus (divisor) */
   do g++;
-  while (g % mod1 != rem1);
+  while (g % eq1[1] != eq1[0]);
   /* now we can skip through the rest of the numbers using the
    * largest modulus as the interval */
-  for (i = g;; i+= mod1)
-    if (i % mod2 == rem2 && i % mod3 == rem3) break;
+  for (i = g;; i+= eq1[1])
+    if (i % eq2[1] == eq2[0] && i % eq3[1] == eq3[0]) break;
   printf("%d\n", i);
   return 0;
 }
