1bb2dd7cc9ee9504dcdff726fa3f0736201738d0
David <pl3onasm@gmail.com>
2022-10-31 19:10:05 +0100

    :tada:

--- Midterms/mid2020/problem5/prob5.c
+++ Midterms/mid2020/problem5/prob5.c
@@ -7,90 +7,50 @@
 #include <stdlib.h>
 
 int checkEmptyCell(int grid[][7], int col) {
-  //returns the row index where first empty cell is found in given column
-  int row = 0;
-  while (grid[row][col] != 0) row++; 
+  //returns row index with first empty cell in given column
+  int row = 5;
+  while (grid[row][col] > 0) row--; 
   return row; 
 }
 
-void fillGrid(int grid[][7]) {
-  //yellow discs are represented by 1, red ones by 2
-  char c; int turn=0, row, col; 
-  while(1) {
-    col = getchar() - '0'; 
-    row = checkEmptyCell(grid, col);
-    if (turn % 2 == 0) grid[row][col] = 1;  //yellow disc
-    else grid[row][col] = 2;  //red disc
-    if ((c = getchar()) == '#') return; //reads comma or #
-    turn++;
+int checkWin (int grid[][7], int row, int col) {
+  int dir[] = {1, 0, 0, 1, 1, 1, -1, 1};
+  for (int i = 0; i < 8; i += 2) {
+    int count = 1;
+    for (int mult = -1; mult <= 1; mult += 2) {
+      int r = row + mult * dir[i];
+      int c = col + mult * dir[i+1];
+      while (r >= 0 && r < 6 && c >= 0 && c < 7 && grid[r][c] == grid[row][col]) {
+        count++;
+        r += mult * dir[i];
+        c += mult * dir[i+1];
+      }
+    }
+    if (count >= 4) return 1;
   }
+  return 0;
 }
 
-char checkGrid(int grid[][7]) {
-  //check rows
-  int c, r, yel, red;
-  for (r=0; r<6; r++) {
-    for (c=0; c<4; c++) {
-      red=yel=0;
-      for (int i=0; i<4; i++) {
-        if (grid[r][c+i] == 1) yel++;
-        else if (grid[r][c+i] == 2) red++;
-        else break;
-      }
-    if (yel == 4) return 'y';
-    if (red == 4) return 'r';
-    }
-  } 
-  //check columns
-  for (c=0; c<7; c++) {
-    for (r=0; r<3; r++) {
-      red=yel=0;
-      for (int i=0; i<4; i++) {
-        if (grid[r+i][c] == 1) yel++;
-        else if (grid[r+i][c] == 2) red++;
-        else break;
-      }
-    if (yel == 4) return 'y';
-    if (red == 4) return 'r';
-    }
-  } 
-  //check diagonals to the right (bottom-up)
-  for (r=0; r<3; r++) {
-    for (c=0; c<4; c++) {
-      yel=red=0;
-      for (int i=0; i<4; i++) {
-        if (grid[r+i][c+i] == 1) yel++;
-        else if (grid[r+i][c+i] == 2) red++;
-        else break;
-      }
-      if (yel == 4) return 'y';
-      if (red == 4) return 'r';
-    }
+int checkGrid(int grid[][7]) {
+  //yellow discs are represented by 1, red ones by 2
+  int turn=0, row, col; 
+  while(scanf("%d,", &col) == 1) {
+    row = checkEmptyCell(grid, col);
+    if (turn % 2 == 0) grid[row][col] = 1; //yellow disc
+    else grid[row][col] = 2;  //red disc
+    if (checkWin(grid, row, col)) return turn % 2;
+    turn++;
   }
-  //check diagonals to the left (bottom-up)
-  for (c=3; c<7; c++) {
-    for (r=0; r<3; r++) {
-      yel=red=0;
-      for (int i=0; i<4; i++) {
-        if (grid[r+i][c-i] == 1) yel++;
-        else if (grid[r+i][c-i] == 2) red++;
-        else break;
-      }
-      if (yel == 4) return 'y';
-      if (red == 4) return 'r';
-    }
-  }
-  return 'u'; 
+  return -1;
 }
 
 int main(int argc, char *argv[]) {
-  int grid[6][7] = {0}; //initializes grid with zeros
-  fillGrid(grid);
-  char c = checkGrid(grid);
+  int grid[6][7] = {0}; // initializes grid with zeros
+  int r = checkGrid(grid);
 
-  if (c=='r') printf("RED\n");
-  else if (c=='y') printf("YELLOW\n");
-  else printf("UNDECIDED\n");
+  if (r > 0) printf("RED\n");
+  else if (r < 0) printf("UNDECIDED\n");
+  else printf("YELLOW\n");
 
   return 0;
 }
